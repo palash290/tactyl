f266d0397146c162b31803b0e570586ea2031517
Palash <palashjain.ctinfotech@gmail.com>
2026-03-24 23:30:10 +0530

    new changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -5,8 +5,8 @@ import { getAnalytics } from "firebase/analytics";
 export const environment = {
       production: false,
       // baseUrl: 'https://tactyl-dev.online/api/auth/',
-      baseUrl: 'https://tactyl.online/api/auth/',
-      // baseUrl: 'http://192.168.1.39:4006/api/',
+      // baseUrl: 'https://tactyl.online/api/auth/',
+      baseUrl: 'http://192.168.1.14:4006/api/',
 
       // Your web app's Firebase configuration
       // For Firebase JS SDK v7.20.0 and later, measurementId is optional
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
       production: true,
       // baseUrl: 'https://tactyl-dev.online/api/auth/',
       baseUrl: 'https://tactyl.online/api/auth/',
-      // baseUrl: 'http://192.168.1.39:4006/api/',
+      // baseUrl: 'http://192.168.1.14:4006/api/',
 
       // Your web app's Firebase configuration
       // For Firebase JS SDK v7.20.0 and later, measurementId is optional
